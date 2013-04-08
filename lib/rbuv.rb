6f1789934e1d69c45da1add520b2ec7db97ac64f
Hanfei Shen <qqshfox@gmail.com>
2013-04-08 23:11:08 +0800

    better singleton class

--- lib/rbuv.rb
+++ lib/rbuv.rb
@@ -3,25 +3,31 @@ require 'rbuv/version'
 require 'rbuv/timer'
 
 module Rbuv
-  def self.run_loop
-    Loop.run
-  end
+  class << self
 
-  def self.stop_loop
-    Loop.stop
-  end
-
-  def self.run
-    Timer.start 0, 0 do
-      yield
+    def run_loop
+      Loop.run
     end
-    self.run_loop
-  end
 
-  def self.run_block
-    Timer.start 0, 0 do
-      yield
+    def stop_loop
+      Loop.stop
     end
-    Loop.run_once
+
+    alias stop stop_loop
+
+    def run
+      Timer.start 0, 0 do
+        yield
+      end
+      self.run_loop
+    end
+
+    def run_block
+      Timer.start 0, 0 do
+        yield
+      end
+      Loop.run_once
+    end
+
   end
 end
